bbb33e35fcb7c8befc852e6e28f78416cebfaab0
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-09-01 11:45:49 +0300

    CREATE PROJECT

---
+++ AvitoTest/AvitoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvitoTestApp.swift
+//  AvitoTest
+//
+//  Created by Janiece Eleonor on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AvitoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
