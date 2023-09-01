82aa84a9100876a35be2523db87e7fcca2a572ae
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-09-01 13:46:34 +0300

    ADD WINDOW NOT WORKING

--- AvitoTest/AppDelegate.swift
+++ AvitoTest/AppDelegate.swift
@@ -9,10 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
