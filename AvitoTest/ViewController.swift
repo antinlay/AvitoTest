9665bf06252c5e7e919cfc5d92507eae9b708fbb
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-09-01 12:01:02 +0300

    DELETE STORYBOARD

---
+++ AvitoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTest
+//
+//  Created by Janiece Eleonor on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
